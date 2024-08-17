205e12529d505eb0ebabd988855c602677c3ed1a
laibasami56 <laibasami9876@gmail.com>
2024-08-18 01:44:04 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+// CHAPTER 4
+
+//1. Declare 3 variables in one statement.
+
+let x = 10, y = 20, z = 30;
+
+
+
+// 2. Declare 5 legal & 5 illegal variable names.
+
+// Legal Variable Names:
+var firstName;
+var age_24;
+var _total;
+var $price;
+var userStatus;
+
+
+
+// Illegal Variable Names:
+// var 2ndPlace ;   (Cannot start with a number)
+// var first-name ;  (Hyphens are not allowed)
+// var class ; (Reserved keyword in JavaScript)
+// var var ; (Reserved keyword in JavaScript)
+// var user name ;  (Spaces are not allowed)
+
+
+
+
